74b47780e53067d0229aecdbb5aa014740f93b4d
sigtot <sigurdtotland@gmail.com>
2020-03-06 22:51:28 +0100

    Make various essential changes

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,5 +4,5 @@ INSTALL_DIR=`pwd`
 cd $HOME
 ln -sf $INSTALL_DIR/.bash_aliases .bash_aliases
 ln -sf $INSTALL_DIR/.bashrc .bashrc
-ln -sf $INSTALL_DIR/.vimrc .vimr
+ln -sf $INSTALL_DIR/.vimrc .vimrc
 ln -sf $INSTALL_DIR/.xinitrc .xinitrc
