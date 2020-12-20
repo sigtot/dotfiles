094fe843c0167b5cdb822f75d552f50649c0dba7
sigtot <sigurdtotland@gmail.com>
2020-12-20 18:36:52 +0100

    Add .Xresources with hidpi settings

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -sf $INSTALL_DIR/.bash_aliases .bash_aliases
 ln -sf $INSTALL_DIR/.bashrc .bashrc
 ln -sf $INSTALL_DIR/.vimrc .vimrc
 ln -sf $INSTALL_DIR/.xinitrc .xinitrc
+ln -sf $INSTALL_DIR/.Xresources
 
 git config --global core.excludesfile "$INSTALL_DIR/.global_gitignore"
